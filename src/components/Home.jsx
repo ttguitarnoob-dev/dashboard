9de9998aa1b7f7914dfd4015f6e4a9c21d4a899c
ttguitarnoob-dev <ttguitarnoob@gmail.com>
2023-05-05 17:38:53 +0000

    created Home and Nav components

---
+++ src/components/Home.jsx
@@ -0,0 +1,7 @@
+
+
+export default function Home(){
+    return <ul>
+        <li><button><a>Button 1</a></button></li>
+    </ul>
+}
---
+++ src/components/Nav.jsx
@@ -0,0 +1,7 @@
+export default function Nav(){
+return <nav>
+    <h1>HazelNet</h1>
+    <button><a href="/">Go Home</a></button>
+</nav>
+
+}
