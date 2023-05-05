6cf928280c73c38cca664714f1393830349b1158
ttguitarnoob-dev <ttguitarnoob@gmail.com>
2023-05-05 14:34:55 +0000

    set up router and add home component

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn to poop
+          Learn to poop even better
         </a>
       </header>
     </div>
