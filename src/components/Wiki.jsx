075550024fe6258d40422b6cce2beff2bf957e1c
ttguitarnoob-dev <ttguitarnoob@gmail.com>
2023-05-05 22:08:31 +0000

    changed width of wiki iframe

--- src/components/Wiki.jsx
+++ src/components/Wiki.jsx
@@ -1,5 +1,5 @@
 export default function Wiki(){
-    return <iframe src="https://www.wikipedia.org/" height="600" width="100%" title="Wikipedia">
+    return <iframe src="https://www.wikipedia.org/" height="600" width="94%" title="Wikipedia">
 
     
     </iframe>
