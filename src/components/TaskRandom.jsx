dcab961431cba7a4a4375b19cfebbb1a95c07715
ttguitarnoob-dev <ttguitarnoob@gmail.com>
2023-06-21 14:59:07 +0000

    finished prettifying task random page

--- src/components/TaskRandom.jsx
+++ src/components/TaskRandom.jsx
@@ -5,7 +5,7 @@ export default function TaskRandom() {
     var [task, setTask] = useState("Your Next Task Awaits!")
 
     const tasks = ["Cat Box", "Sweep", "Poo", "Pet Your Cat", "Pet Your Husband"]
-    
+
     function Spin() {
         const length = tasks.length
         task = tasks[Math.floor(Math.random() * length)]
@@ -15,9 +15,11 @@ export default function TaskRandom() {
 
 
     return <div className="task-random">
-        <div className="spinner">
-        <h2>Spin the not wheel to see your next task!</h2>
-        <button onClick={Spin}>{task}</button>
+        <div className="spinner-container">
+            <div className="spinner">
+                <h2>Spin the not wheel to see your next task!</h2>
+                <button className="random-button" onClick={Spin}>{task}</button>
+            </div>
         </div>
     </div>
 }
