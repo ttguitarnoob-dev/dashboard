42a53b50dcc4af5c7382d879418f3d5d480f1969
ttguitarnoob-dev <ttguitarnoob@gmail.com>
2023-06-21 15:15:10 +0000

    further tweaking to prettiness

--- src/components/TravNav.jsx
+++ src/components/TravNav.jsx
@@ -1,11 +1,8 @@
 export default function TravNav() {
     return <nav className="trav-header">
         <div className="travnav-items">
-
-            <a href='#'><h2>KittyCottage</h2></a>
             <a href='/kiara'><h2 className="travnav">Kiara</h2></a>
             <a href='/'><h2>Travis</h2></a>
-
         </div>
     </nav>
 
