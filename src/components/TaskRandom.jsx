690ed707c60c216f3ce257e320315142810a76c5
ttguitarnoob-dev <ttguitarnoob@gmail.com>
2023-07-05 10:32:46 -0500

    ya I didn't need that

--- src/components/TaskRandom.jsx
+++ src/components/TaskRandom.jsx
@@ -48,7 +48,6 @@ export default function TaskRandom() {
             const deletedTask = await response.json()
             const updatedTasklist = tasklist.filter(task => task._id !== deletedTask._id)
             setTaskList(updatedTasklist)
-            handleFetch()
         } catch(err) {
             console.log('Something broke when you tried to delete that:', err)
         }
