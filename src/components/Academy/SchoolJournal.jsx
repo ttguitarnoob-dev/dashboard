365964658f22e856fc47abfabaa972416f2b31fc
ttguitarnoob-dev <ttguitarnoob@gmail.com>
2023-07-08 13:58:15 -0500

    journal page structure stubbed out

--- src/components/Academy/SchoolJournal.jsx
+++ src/components/Academy/SchoolJournal.jsx
@@ -2,13 +2,18 @@ export default function SchoolJournal() {
     console.log("smellass")
 
     return <div>
+        <h2>Learning Journal</h2>
+        <div>
+            <a href="/academy/journal/new">New Journal</a>
+        </div>
 
-<h2>Learning Journal</h2>
-<ul>
-    <li>Journal 1</li>
-    <li>Journal 2</li>
-    <li>Journal 3</li>
-</ul>
+        <ul>
+            <li>Journal 1</li>
+            <li>Journal 2</li>
+            <li>Journal 3</li>
+        </ul>
+
+
 
     </div>
 }
