653d890243eadebf8b603285ef34873320c7829c
ttguitarnoob-dev <ttguitarnoob@gmail.com>
2023-07-10 13:37:46 -0500

    fetch working for journal entries list

--- src/components/Academy/SchoolJournal.jsx
+++ src/components/Academy/SchoolJournal.jsx
@@ -1,5 +1,44 @@
+import { useState, useEffect } from "react"
+
+
 export default function SchoolJournal() {
-    console.log("smellass")
+
+    var [journalList, setJournalList] = useState([])
+
+    const smell = new Date().toDateString()
+
+
+    console.log("smellass", smell)
+
+    const journalURL = "https://api.ttguitarnoob.cloud/journals"
+
+    async function handleFetch() {
+        try {
+            const URL = journalURL
+            const options = {
+                method: "GET"
+            }
+            
+            const response = await fetch(URL, options)
+            const results = await response.json()
+            var array = results.map(journal => journal)
+            console.log("poohole", array)
+
+            setJournalList(array)
+
+        } catch (err) {
+            console.log('something bad happened when fetching', err)
+        }
+
+    }
+
+    //handleFetch function call
+    useEffect(() => {
+        handleFetch()
+    }, [])
+
+
+
 
     return <div>
         <h2>Learning Journal</h2>
@@ -7,16 +46,14 @@ export default function SchoolJournal() {
             <a href="/academy/journal/new">New Journal</a>
         </div>
 
-        <table>
-            <thead>
-                <th>Journal</th>
-                <th>Date</th>
-            </thead>
-            <tbody>
-                <td>Journal 1</td>
-                <td>8/8/2023</td>
-            </tbody>
-        </table>
+        <h3>Look back at your old journals!</h3>
+        <div>
+            <ul>
+                {journalList && journalList.map((journal, index) => (
+                    <a href={`${journalURL}/${journal._id}`} key={journal._id}><li>{journal.date}</li></a>
+                ))}
+            </ul>
+        </div>
 
 
 
