3242567017c1b8b1efa3c3651343bb37ab121963
ttguitarnoob-dev <ttguitarnoob@gmail.com>
2023-07-28 08:44:55 -0600

    r

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 
 import './App.css';
-import React, { useEffect, useState } from "react"
+import React from "react"
 import { Route, Routes } from 'react-router-dom';
-import Hazel from './components/Hazel';
-import HazelNav from './components/HazelNav';
-import Wiki from './components/Wiki';
-import Poke from './components/Poke';
-import Yt from './components/Yt';
 import TravNav from './components/TravNav';
 import Trav from './components/Trav';
 import Budget from './components/Budget';
-import NoGame from './components/NoGame';
 import Kiara from './components/Kiara';
 import TaskRandom from './components/TaskRandom';
 import Academy from './components/Academy/Academy';
@@ -40,6 +34,7 @@ function App() {
 
       const response = await fetch(delURL, options)
       const results = await response.json()
+      console.log('result?', results)
 
 
     } catch (err) {
@@ -54,61 +49,61 @@ function App() {
 
 
 
-  let Component
-  let Navigation
-  switch (window.location.pathname) {
-    case "/":
-      Component = <Trav />
-      Navigation = <TravNav />
-      break
-    case "/hazel":
-      Component = <Hazel />
-      Navigation = <HazelNav />
-      break
-    case "/wiki":
-      Component = <Wiki />
-      Navigation = <HazelNav />
-      break
-    case "/poke":
-      Component = <Poke />
-      Navigation = <HazelNav />
-      break
-    case "/youtube":
-      Component = <Yt />
-      Navigation = <HazelNav />
-      break
-    case "/no-game":
-      Component = <NoGame />
-      Navigation = <HazelNav />
-      break
-    case "/budget":
-      Component = <Budget />
-      Navigation = <TravNav />
-      break
-    case "/kiara":
-      Component = <Kiara />
-      Navigation = <TravNav />
-      break
-    case "/task-random":
-      Component = <TaskRandom />
-      Navigation = <TravNav />
-      break
-    case "/academy":
-      Component = <Academy />
-      Navigation = <HazelNav />
-      break
-    case "/academy/journal":
-      Component = <SchoolJournal />
-      Navigation = <HazelNav />
-      break
-    case "/academy/journal/new":
-      Component = <NewJournal />
-      Navigation = <HazelNav />
-      break
-    case "/academy/journal/:id":
-      Component = <JournalDetails />
-      Navigation = <HazelNav />
-  }
+  // let Component
+  // let Navigation
+  // switch (window.location.pathname) {
+  //   case "/":
+  //     Component = <Trav />
+  //     Navigation = <TravNav />
+  //     break
+  //   case "/hazel":
+  //     Component = <Hazel />
+  //     Navigation = <HazelNav />
+  //     break
+  //   case "/wiki":
+  //     Component = <Wiki />
+  //     Navigation = <HazelNav />
+  //     break
+  //   case "/poke":
+  //     Component = <Poke />
+  //     Navigation = <HazelNav />
+  //     break
+  //   case "/youtube":
+  //     Component = <Yt />
+  //     Navigation = <HazelNav />
+  //     break
+  //   case "/no-game":
+  //     Component = <NoGame />
+  //     Navigation = <HazelNav />
+  //     break
+  //   case "/budget":
+  //     Component = <Budget />
+  //     Navigation = <TravNav />
+  //     break
+  //   case "/kiara":
+  //     Component = <Kiara />
+  //     Navigation = <TravNav />
+  //     break
+  //   case "/task-random":
+  //     Component = <TaskRandom />
+  //     Navigation = <TravNav />
+  //     break
+  //   case "/academy":
+  //     Component = <Academy />
+  //     Navigation = <HazelNav />
+  //     break
+  //   case "/academy/journal":
+  //     Component = <SchoolJournal />
+  //     Navigation = <HazelNav />
+  //     break
+  //   case "/academy/journal/new":
+  //     Component = <NewJournal />
+  //     Navigation = <HazelNav />
+  //     break
+  //   case "/academy/journal/:id":
+  //     Component = <JournalDetails />
+  //     Navigation = <HazelNav />
+  // }
   return (
     // <>
 
@@ -124,14 +119,12 @@ function App() {
         <Route path='/academy' element={<Academy />} />
         <Route path='/kiara' element={<Kiara />} />
         <Route path='/' element={<Trav />} />
-        <Route path='/poke' element={<Poke />} />
+        <Route path='/budget' element={<Budget />} />
         <Route path='/academy/journal/' element={<SchoolJournal />} />
         <Route path='/countdown/new' element={<NewCountdown />} />
         <Route path='/countdown/:id' element={<Countdown HandleDelete={HandleDelete} />} />
         <Route path='/countdown' element={<CountdownList />} />
         <Route path='/task-random' element={<TaskRandom />} />
-
-
       </Routes>
     </div>
 
