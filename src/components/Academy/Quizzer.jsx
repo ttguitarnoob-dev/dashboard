06916af03a483087481adb32e1eecd60a8d03c05
ttguitarnoob-dev <ttguitarnoob@gmail.com>
2023-09-06 16:22:01 -0500

    fixed data structure for quizzes

--- src/components/Academy/Quizzer.jsx
+++ src/components/Academy/Quizzer.jsx
@@ -5,57 +5,61 @@ import { useFetcher } from "react-router-dom"
 export default function Quizzer(props) {
 
     var initialInput = {
-        title: "omgsmelllBUTTTT",
-        subject: "larnin'",
+        title: "SMELLASS",
+        subject: "looornan'",
         score: 0,
-        questions: ["question 1", "omg"]
-
-    }
-// const { HandleGet } = props
-
-// const data = useEffect(() => {
-//     HandleGet("http://localhost:8000/quizzes")
-// }, [])
-
-const handleSubmit = (e) => {
-    e.preventDefault()
-    console.log("Submitted", initialInput)
-    const formBody = Object.keys(initialInput).map(key =>
-        encodeURIComponent(key) + '=' +
-        encodeURIComponent(initialInput[key])).join('&')
-    console.log('formbody', formBody)
-    HandlePost(initialInput)
-
-}
-
-const HandlePost = async (data) => {
-    console.log('start of post function', data.title)
-    const postURL = "http://localhost:8000/quizzes"
-    var formBody = new FormData()
-    formBody.set("title", data.title)
-    formBody.set("subject", data.subject)
-    formBody.set("score", data.score)
-    formBody.set("questions", data.questions)
-    console.log('pllezzz', formBody)
-    try{
-        const options = {
-            method: "POST",
-            body: JSON.stringify(formBody),
-            mode: "cors",
-            headers: {
-                'Content-Type': 'application/json'
-            }
+        questions: [{
+            question: "what is your favorite color",
+            answer: "a",
+            choice: "a",
+            choices: { a: "orange", b: "your other mom", c: "black", d: "laksdjf" }
+        },
+        {
+            question: "what is your favorite FOOD",
+            answer: "b",
+            choice: "a",
+            choices: { a: "orange", b: "your other mom", c: "MACARONI AND CHEESE", d: "oidjfoi" }
         }
-      const response = await fetch(postURL, options)
-      const results = await response.text()
-      console.log('data', data)
-      console.log('results', results)
-      return results
+        ]
 
-    } catch(err) {
-      console.log("something broke when posting", err)
     }
-  }
+    // const { HandleGet } = props
+
+    // const data = useEffect(() => {
+    //     HandleGet("http://localhost:8000/quizzes")
+    // }, [])
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log("Submitted", initialInput)
+
+        HandlePost(initialInput)
+
+    }
+
+    const HandlePost = async (data) => {
+        console.log('start of post function', data.title)
+        const postURL = "http://localhost:8000/quizzes"
+
+        try {
+            const options = {
+                method: "POST",
+                body: JSON.stringify(data),
+                mode: "cors",
+                headers: {
+                    'Content-type': 'application/json'
+                }
+            }
+            const response = await fetch(postURL, options)
+            const results = await response.json()
+            console.log('data', data)
+            console.log('results', results)
+            return results
+
+        } catch (err) {
+            console.log("something broke when posting", err)
+        }
+    }
 
 
 
