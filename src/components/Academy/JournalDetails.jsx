b0180f3a5e852d7d95d8ffd23a224b42bb34dafc
ttguitarnoob-dev <ttguitarnoob@gmail.com>
2023-09-29 17:14:46 -0500

    journal details page fetch created

--- src/components/Academy/JournalDetails.jsx
+++ src/components/Academy/JournalDetails.jsx
@@ -1,10 +1,34 @@
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 export default function JournalDetails() {
     const { id } = useParams()
-    console.log('de params', id)
+    const URL = `https://api.ttguitarnoob.cloud/kizzi-journals/${id}`
+
+    //state
+    const [journal, setJournal] = useState()
+
+    async function handleFetch(){
+        const options = {
+            method: "GET"
+        }
+
+        try {
+            const response = await fetch(URL, options)
+            const data = response.json()
+            console.log('dada', data)
+            setJournal(data)
+            console.log(journal)
+        } catch(err){
+            console.log('something tremendously awful happened when fetching the journal entry', err)
+        }
+    }
+
+    useEffect(() => {
+        handleFetch()
+    }, [])
 
     return <div>
-        <h2>Helloooo {id}</h2>
+        <h2>Helloooo </h2>
     </div>
 }
